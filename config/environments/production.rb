40b969dbceac835fb799061961cbe98385e9314e
Stephan White <sgradywhite@yahoo.com>
2016-10-25 22:18:24 +0000

    Update development and production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'herokuapp.com'
+  host = 'hidden-coast-79154.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
